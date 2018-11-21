85c23096034f333ddb0a9e45ed87884ab97225c7
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-22 02:26:30 +0700

    Specify cors origin

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ database_connect.then((db) => {
 })
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}))
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(bodyPatser.json())
